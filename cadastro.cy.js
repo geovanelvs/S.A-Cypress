64d537764286d49659ffb6e34f4b504bf19f02cc
geovane <0001157770@senaimgaluno.com.br>
2025-12-05 18:24:28 -0300

    Teste corrigido com bloqueio de e-mail falso

--- cadastro.cy.js
+++ cadastro.cy.js
@@ -21,11 +21,25 @@ describe("Página de Cadastro - Poke+", () => {
     cy.get("#email").type("gegeo@gmail.com");
     cy.get("#senhaCadastro").type("123456");
     cy.get("#confirmarSenha").type("987654");
+
     cy.get("button[type='submit']").click();
 
     cy.get("#mensagemCadastro")
       .should("be.visible")
       .and("contain", "senhas");
+  });
+
+  it("deve bloquear envio com e-mail inválido", () => {
+    cy.get("#nome").type("Geovane");
+    cy.get("#email").type("emailinvalido");
+    cy.get("#senhaCadastro").type("123456");
+    cy.get("#confirmarSenha").type("123456");
+
+    cy.get("button[type='submit']").click();
+
+    cy.get("#email").then(($input) => {
+      expect($input[0].checkValidity()).to.be.false;
+    });
   });
 
   it("deve permitir preencher todos os campos corretamente", () => {
@@ -38,8 +52,7 @@ describe("Página de Cadastro - Poke+", () => {
     cy.get("#mensagemCadastro").should("not.have.text", "");
   });
 
-  it('deve funcionar o botão de cadastro', () => {
-    cy.get('#cadastroForm button.btn').click();
+  it("deve funcionar o botão de cadastro", () => {
+    cy.get("#cadastroForm button.btn").click();
   });
-
 });
